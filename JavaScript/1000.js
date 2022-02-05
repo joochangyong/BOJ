7fb75e03c35558e455c995e9acc7eb422847eb13
Joo ChangYong <51350374+joochangyong@users.noreply.github.com>
2022-02-05 06:59:58 +0000

    java, javascript 추가

---
+++ JavaScript/1000.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split(' ');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
---
+++ JavaScript/2558.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
